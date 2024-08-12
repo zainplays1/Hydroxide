c185008e21dabb06febf145521d3348885aa7532
zainplays1 <71414732+zainplays1@users.noreply.github.com>
2024-08-11 23:38:27 -0400

    Update ScriptScanner.lua

--- modules/ScriptScanner.lua
+++ modules/ScriptScanner.lua
@@ -1,17 +1,1 @@
-local ScriptScanner = {}
-local LocalScript = import("objects/LocalScript")
 
-local rInstance)
-                end
-            end
-        end
-    end
-
-    return scripts
-end
-
-
-
-ScriptScanner.RequiredMethods = requiredMethods
-ScriptScanner.Scan = scan
-return ScriptScanner

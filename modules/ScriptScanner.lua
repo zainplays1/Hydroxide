e953e8851f037d2110d55b13e4b8db794866fd6b
zainplays1 <71414732+zainplays1@users.noreply.github.com>
2024-08-11 23:38:42 -0400

    Update ScriptScanner.lua

--- modules/ScriptScanner.lua
+++ modules/ScriptScanner.lua
@@ -1,1 +1,47 @@
+local ScriptScanner = {}
+local LocalScript = import("objects/LocalScript")
 
+local requiredMethods = {
+    ["getGc"] = true,
+    ["getSenv"] = true,
+    ["getProtos"] = true,
+    ["getConstants"] = true,
+    ["getScriptClosure"] = true,
+    ["isXClosure"] = true
+}
+
+local function scan(query)
+    local scripts = {}
+    query = query or ""
+
+    for _i, v in pairs(getGc()) do
+        if type(v) == "function" and not isXClosure(v) then
+            local success, script = pcall(function()
+                local s = rawget(getfenv(v), "script")
+                if typeof(s) == "Instance" and s:IsA("LocalScript") then
+                    -- Attempt a basic operation to filter out problematic scripts
+                    return getScriptClosure(s), s
+                end
+            end)
+
+            if success and script then
+                local closure, scriptInstance = script[1], script[2]
+                local envSuccess = pcall(function()
+                    return getsenv(scriptInstance)
+                end)
+
+                if envSuccess then
+                    scripts[scriptInstance] = LocalScript.new(scriptInstance)
+                end
+            end
+        end
+    end
+
+    return scripts
+end
+
+
+
+ScriptScanner.RequiredMethods = requiredMethods
+ScriptScanner.Scan = scan
+return ScriptScanner
